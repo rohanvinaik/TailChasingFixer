905fdf7da184a46b29209a8215a3bdb85cf743e1
Rohan Vinaik <rohanpvinaik@gmail.com>
2025-07-23 23:30:17 -0400

    chore: Clean up repository structure
    - Remove unnecessary shell scripts and command files
    - Remove temporary output files and Python cache
    - Remove .pypirc template
    - Enhance .gitignore for better hygiene
    - Make scripts executable
    - Prepare for clean GitHub deployment

--- run_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Make the script executable
-chmod +x /Users/rohanvinaik/github/TailChasingFixer/push_advanced_features.sh
-
-# Run it
-/Users/rohanvinaik/github/TailChasingFixer/push_advanced_features.sh
